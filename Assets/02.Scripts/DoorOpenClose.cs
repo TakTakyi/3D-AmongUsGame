b36d205d3c2627896a7c717938ad94eb790267a2
TakTakyi <rlaclxortl@naver.com>
2022-12-14 18:45:49 +0900

    문짝 테스트중

---
+++ Assets/02.Scripts/DoorOpenClose.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenClose : MonoBehaviour
+{
+    bool Dopenclose;
+
+    public GameObject LDoor;
+    Vector3 LDPos;
+    public GameObject RDoor;
+    Vector3 RDPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dopenclose = false;
+        LDPos = LDoor.transform.position;
+        RDPos = RDoor.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Dopenclose == true)
+        {
+            if (LDoor.transform.position.y >= -1.0f && RDoor.transform.position.y >= -1.0f)
+            {
+                Debug.Log(LDoor.transform.position);
+                Debug.Log(RDoor.transform.position);
+
+                LDPos.y = -3.5f;
+                RDPos.y = -3.5f;
+                
+                LDoor.transform.Translate(LDPos);
+                RDoor.transform.Translate(RDPos);
+            }
+        }
+
+        if (Dopenclose == false)
+        {
+            if (LDoor.transform.position.y <= -3.5f && RDoor.transform.position.y <= -3.5f)
+            {
+                Debug.Log(LDoor.transform.position);
+                Debug.Log(RDoor.transform.position);
+
+                LDPos.y = 0.0f;
+                RDPos.y = 0.0f;
+
+                LDoor.transform.Translate(LDPos);
+                RDoor.transform.Translate(RDPos);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Dopenclose = true;
+        Debug.Log(Dopenclose);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Dopenclose = false;
+        Debug.Log(Dopenclose);
+    }
+}
