1f06f04dba4cbb88d13d86196e24584d3dfb9b92
TakTakyi <rlaclxortl@naver.com>
2023-04-06 22:38:10 +0900

    퀘스트 추가중

--- Assets/02.Scripts/Quest_Scripts/EmptyTrash/Empty_Trash.cs
+++ Assets/02.Scripts/Quest_Scripts/EmptyTrash/Empty_Trash.cs
@@ -7,12 +7,36 @@ public class Empty_Trash : QuestObjects
     // Start is called before the first frame update
     void Start()
     {
-        
+        Quest_Available = true;
+        if (m_ClostBtn != null)
+        {
+            m_ClostBtn.onClick.AddListener(CloseBtnFunc);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void OnOffUICanvas(bool OnOff)
+    {
+        base.OnOffUICanvas(OnOff);
+    }
+
+    public override void OnOffQuestCanvas(bool OnOff)
+    {
+        base.OnOffQuestCanvas(OnOff);
+    }
+
+    public override void CloseBtnFunc()
+    {
+        base.CloseBtnFunc();
+    }
+
+    public override void QuestComp()
+    {
+        base.QuestComp();
     }
 }
